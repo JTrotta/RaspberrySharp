5d1958737e02d96ebb7ba7d90412392b3fe5373e
Dominik Schmidt <dominik@logview.info>
2018-04-17 05:06:30 +0200

    Added Sample Program for I2CDetect

---
+++ Samples/Advanced/I2CDetect/Program.cs
@@ -0,0 +1,39 @@
+﻿// **************************************************************
+// Project     : I2CDetect
+// Filename    : Program.cs
+// Details     : https://github.com/JTrotta/RaspberrySharp
+// **************************************************************
+// Github Wiki : https://github.com/JTrotta/RaspberrySharp/wiki
+// DataSheet   : 
+// **************************************************************
+// Links       : 
+// **************************************************************
+// Notes       : 
+
+#region Usings
+
+using System;
+using RaspberrySharp.IO.InterIntegratedCircuit;
+
+#endregion
+
+namespace I2CDetect
+{
+    /// <summary>
+    /// Sample for reading the I2C Bus
+    /// </summary>
+    public class Program
+    {
+        public static I2cDetect _I2CDetect;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I2C Detect");
+            Console.WriteLine("===============");
+
+            _I2CDetect = new I2cDetect();
+            var list = _I2CDetect.Detect();
+            foreach (var item in list) Console.WriteLine(item);
+        }
+    }
+}
